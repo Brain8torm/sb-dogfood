6b491ea2d499ba9c653b2b4d4cfb32999252b731
Brain8torm <brainstorm@programist.ru>
2023-03-16 23:13:21 +0300

    Оптимизированы изображения

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -7,7 +7,7 @@ export function Card({ name, price, discount, wight, description, picture }) {
 
   const discount_price = Math.round(price - (price * discount) / 100);
 
-  
+
   return (
     <article className={classNames(styles.item, 'card')}>
       <div className={classNames(styles.sticky, styles.sticky__type_top_left)}>
@@ -15,11 +15,15 @@ export function Card({ name, price, discount, wight, description, picture }) {
       </div>
       <div className={classNames(styles.sticky, styles.sticky__type_top_right)}>
         <button className={classNames(styles.favorite)}>
-          <img src={likeIcon} alt="" className={classNames(styles.favorite__icon)} />
+          <img src={likeIcon} alt="" className={classNames(styles.favorite__icon)} width="24px" height="24px" />
         </button>
       </div>
       <a href="/" className={classNames(styles.link)}>
-        <img src={picture} alt={name} className={classNames(styles.image)} />
+        <img src={picture} alt={name} className={classNames(styles.image)} sizes="
+    (max-width: 500px) calc(100vw - 2rem), 
+    (max-width: 700px) calc(100vw - 6rem),
+    calc(100vw - 9rem - 200px)
+  " />
         <div className={classNames(styles.desc)}>
           {discount !== 0 ? (
             <>
@@ -29,10 +33,10 @@ export function Card({ name, price, discount, wight, description, picture }) {
           ) : (
             <span className={classNames(styles.price)}>{price}&nbsp;₽</span>
           )}
-            <span className={classNames(styles.wight)}>{wight}</span>
-        <h3 className={classNames(styles.name)}>{name}</h3>
-      </div>
-    </a>
+          <span className={classNames(styles.wight)}>{wight}</span>
+          <h3 className={classNames(styles.name)}>{name}</h3>
+        </div>
+      </a>
     </article >
   )
 }
--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,11 +1,23 @@
 import styles from './logo.module.css';
 import classNames from 'classnames';
 import logoSrc from './images/logo.svg';
+import logoMobileSrc from './images/logo-mobile.svg';
 
 export function Logo() {
   return (
     <a href="/" className={classNames(styles.link)}>
-      <img src={logoSrc} alt="Логотип" className={classNames(styles.image)} />
+      <picture>
+        <source
+          srcSet={logoMobileSrc}
+          media="(max-width: 700px)"
+          width="40px"
+        />
+        <img
+          src={logoSrc}
+          alt="Логотип"
+          className={classNames(styles.image)}
+        />
+      </picture>
     </a>
   )
 }

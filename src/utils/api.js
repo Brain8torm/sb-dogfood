e038f84335ea6b13285106f544bd06c307b81269
Brain8torm <brainstorm@programist.ru>
2023-03-20 23:46:03 +0300

    Создан класс Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,15 @@
 class Api {
-    
+    #baseUrl;
+    #headers;
+
+    constructor({ baseUrl, headers }) {
+        thid.#baseUrl = baseUrl;
+        this.#headers = headers;
+    }
+
+    #getResponse(res) {
+        return res.ok ? res.json() : res.json().then(err => Promise.reject(err));
+    }
+
+
 }

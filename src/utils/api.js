fbd5790e70e887f056d7cd9518eaedd216822296
Brain8torm <brainstorm@programist.ru>
2023-03-20 23:49:01 +0300

    Создан экземпляр класса Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,3 +13,13 @@ class Api {
 
 
 }
+
+const api = new Api({
+    baseUrl: 'https://api.react-learning.ru',
+    headers: {
+        'content-type': 'application/json',
+        authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEwN2UwOGFhMzk3MTIxODM4ZjI4ZGEiLCJncm91cCI6Imdyb3VwLTExIiwiaWF0IjoxNjc4ODAyNDQ2LCJleHAiOjE3MTAzMzg0NDZ9.NuCTE98G8Un3lOPytdymhzHM-BGkhlHGruCeCjFNK0c'
+    }
+});
+
+export default api;

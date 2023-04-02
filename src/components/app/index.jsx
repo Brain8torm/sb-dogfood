afb9aafbdcc8c2dc39dcfad83d994401897d86c0
Brain8torm <brainstorm@programist.ru>
2023-04-02 13:08:55 +0300

    Добавлен роутинг

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -9,6 +9,8 @@ import api from '../../utils/api';
 import { CatalogPage } from '../../pages/catalog-page';
 import { ProductPage } from '../../pages/product-page';
 import FaqPage from '../../pages/faq-page';
+import { Route, Routes } from 'react-router';
+import { NotFoundPage } from '../../pages/not-fond-page';
 
 
 export function App() {
@@ -85,18 +87,30 @@ export function App() {
   return (
     <>
       <Header user={currentUser} onUserUpdate={handleUserUpdate}>
-        <Logo />
-        <Search
-          handleInputChange={handleInputChange}
-          handleFormSubmit={handleFormSubmit}
-          handleSearchEscClick={handleSearchEscClick}
-          searchQuery={searchQuery}
-        />
+        <Routes>
+          <Route path='/' element={
+            <>
+              <Logo />
+              <Search
+                handleInputChange={handleInputChange}
+                handleFormSubmit={handleFormSubmit}
+                handleSearchEscClick={handleSearchEscClick}
+                searchQuery={searchQuery}
+              />
+            </>
+          } />
+          <Route path='*' element={<Logo href='/' />} />
+        </Routes>
       </Header>
       <main className='content container'>
-        <FaqPage/>
-        <ProductPage />
-        <CatalogPage cards={cards} handleProductLike={handleProductLike} currentUser={currentUser} isLoading={isLoading} />
+        <Routes>
+          <Route path='/' element={<CatalogPage cards={cards} handleProductLike={handleProductLike} currentUser={currentUser} isLoading={isLoading} />} />
+          <Route path='/faq' element={<FaqPage />} />
+          <Route path='/product/:productID' element={<ProductPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+
+        
       </main>
       <Footer />
     </>
--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -2,22 +2,43 @@ import styles from './logo.module.css';
 import classNames from 'classnames';
 import logoSrc from './images/logo.svg';
 import logoMobileSrc from './images/logo-mobile.svg';
+import { Link } from 'react-router-dom';
 
-export function Logo() {
+export function Logo({ className, href, ...props }) {
+  const hrefValue = href ? href : null;
+
   return (
-    <a href="/" className={classNames(styles.link)}>
-      <picture>
-        <source
-          srcSet={logoMobileSrc}
-          media="(max-width: 700px)"
-          width="40px"
-        />
-        <img
-          src={logoSrc}
-          alt="Логотип"
-          className={classNames(styles.image)}
-        />
-      </picture>
-    </a>
+    hrefValue
+      ?
+      <Link replace to={{ pathname: hrefValue }} className={classNames(styles.link, { className: !!className })} {...props}>
+        <picture>
+          <source
+            srcSet={logoMobileSrc}
+            media="(max-width: 700px)"
+            width="40px"
+          />
+          <img
+            src={logoSrc}
+            alt="Логотип"
+            className={classNames(styles.image)}
+          />
+        </picture>
+      </Link>
+      :
+      <span className={classNames(styles.link, className)}>
+                <picture>
+          <source
+            srcSet={logoMobileSrc}
+            media="(max-width: 700px)"
+            width="40px"
+          />
+          <img
+            src={logoSrc}
+            alt="Логотип"
+            className={classNames(styles.image)}
+          />
+        </picture>
+      </span>
+
   )
 }
